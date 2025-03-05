26c08c47a31f2367f704732ef7e2b19a88dd0f14
joonwoo595 <jonbeo312@gmail.com>
2025-03-05 17:07:55 +0900

    1004

--- src/main/java/com/mycompany/teamproject9/TeamProject9Application.java
+++ src/main/java/com/mycompany/teamproject9/TeamProject9Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeamProject9Application {
+public class  TeamProject9Application {
 
     public static void main(String[] args) {
         SpringApplication.run(TeamProject9Application.class, args);
